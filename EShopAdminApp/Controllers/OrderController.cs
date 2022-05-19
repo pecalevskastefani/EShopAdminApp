537aba32045f46d06fa89f9bee32613cfa0ac2e7
Stefani <stefpecal@gmail.com>
2022-05-19 18:09:44 +0200

    is 9

--- EShopAdminApp/Controllers/OrderController.cs
+++ EShopAdminApp/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using EShopAdminApp.Models;
+﻿using ClosedXML.Excel;
+using EShopAdminApp.Models;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -83,6 +84,48 @@ namespace EShopAdminApp.Controllers
 
 
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvocie.pdf");
+        }
+        
+        [HttpGet]
+        public IActionResult ExportAllOrders()
+        {
+            string fileName = "Orders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            using(var workbook = new XLWorkbook()) //so ova praavime excel dokument so razlicni sheetovi
+            {
+                IXLWorksheet worksheet= workbook.Worksheets.Add("All Orders"); //imeto na sheetot
+                worksheet.Cell(1, 1).Value= "Order Id"; //1redica,1 kolona
+                worksheet.Cell(1, 2).Value = "Customer Email";
+                //treba da gi dodavame i produktite toa go pravime so api
+                HttpClient client = new HttpClient();
+                string URL = "https://localhost:44300/api/admin/GetAllActiveOrders";
+                HttpResponseMessage response = client.GetAsync(URL).Result;
+
+                var result = response.Content.ReadAsAsync<List<Order>>().Result; //site naracki
+
+                for(int i = 1; i <= result.Count(); i++)
+                {
+                    var item = result[i-1]; //da pocne od 0 a ne od 1 kako vo forot
+                    worksheet.Cell(i + 1, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i + 1, 2).Value = item.OrderBy;
+
+                    for(int p = 0; p <item.Products.Count() ; p++)
+                    {
+                        worksheet.Cell(1, p + 3).Value = "Product " + (p + 1);
+                        worksheet.Cell(i + 1, p + 3).Value = item.Products.ElementAt(p).Product.ProductName;
+                    }
+                }
+
+                //da zacuvame treba
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, contentType, fileName); //sodrzina, tip eksel, i ime na file
+                }
+
+            }
+
         }
     }
 }
